52c645fdf927861f5acad428f035d2a853662c65
Mdounia <maziane.dounia@gmail.com>
2021-03-04 19:38:54 +0100

    second commit

---
+++ TestGit/src/test/TestMainGreetingService.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestMainGreetingService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test 3");
+	}
+
+}
